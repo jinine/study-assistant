9e4fe033eb0fb6af84bb0e40adf8ab5b60c99d23
Tristan Engen <jinine@jinine.localdomain>
2024-11-04 03:04:51 -0800

    update user seed

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,14 +1,15 @@
---Create Users Table
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,                                  -- Auto-incrementing ID
-    username VARCHAR(50) NOT NULL UNIQUE,                  -- Unique username, max length of 50
-    email VARCHAR(100) NOT NULL UNIQUE,                     -- Unique email address, max length of 100
-    first_name VARCHAR(50),                                 -- First name, max length of 50
-    last_name VARCHAR(50),                                  -- Last name, max length of 50
-    encrypted_pass VARCHAR(128) NOT NULL,                   -- Encrypted password, max length of 128
-    profile_picture_url VARCHAR(255),                        -- URL of the user's profile picture, max length of 255
-    is_active BOOLEAN DEFAULT TRUE,                          -- User account active status
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,         -- Timestamp of account creation
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,      -- UUID, default to generated UUID
+    username VARCHAR(50) NOT NULL UNIQUE,                 -- Unique username, max length of 50
+    email VARCHAR(100) NOT NULL UNIQUE,                    -- Unique email address, max length of 100
+    first_name VARCHAR(50),                                -- First name, max length of 50
+    last_name VARCHAR(50),                                 -- Last name, max length of 50
+    encrypted_pass VARCHAR(128) NOT NULL,                  -- Encrypted password, max length of 128
+    profile_picture_url VARCHAR(255),                       -- URL of the user's profile picture, max length of 255
+    is_active BOOLEAN DEFAULT TRUE,                         -- User account active status
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,        -- Timestamp of account creation
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP          -- Timestamp of last update
 );
 
