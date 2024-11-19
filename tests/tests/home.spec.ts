e0a4de61ac8b8986c45ee28cf38e92677001edf8
Tristan Engen <jinine@jinine.localdomain>
2024-11-18 16:29:07 -0800

    add tests

---
+++ tests/tests/home.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+// test('basic test', async ({ page }) => {
+//   await page.goto('https://example.com');
+
+//   const title = await page.title();
+//   expect(title).toBe('Example Domain');
+
+//   const heading = await page.textContent('h1');
+//   expect(heading).toBe('Example Domain');
+// });
