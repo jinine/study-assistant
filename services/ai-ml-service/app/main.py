c7c641c3103631824548b72109f4a8f5f62c4e81
Tristan Engen <jinine@jinine.localdomain>
2024-11-18 17:05:03 -0800

    remove unnecessary imports

--- services/ai-ml-service/app/main.py
+++ services/ai-ml-service/app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from app.routes import register_routes
-from app.models import db
 import os
 from dotenv import load_dotenv
 
@@ -12,9 +11,6 @@ app = Flask(__name__)
 # Configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize the database
-db.init_app(app)
 
 # Register routes
 register_routes(app)
